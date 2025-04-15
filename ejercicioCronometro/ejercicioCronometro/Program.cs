c5df81bef737517cceebe331a0361b683a990305
Julián Ferrari <julianferrarijuli@gmail.com>
2025-04-15 11:08:51 -0300

    Añadidos Archivos Ejercicios 1, 2 y 3 POO

---
+++ ejercicioCronometro/ejercicioCronometro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ejercicioCronometro
+{
+    class Cronometro
+    {
+        public int segundos;
+        public int minutos;
+
+        public Cronometro()
+        {
+            segundos = 0;
+            minutos = 0;
+        }
+        public void Reiniciar()
+        {
+            segundos = 0;
+            minutos = 0;
+        }
+        public void IncrementarTiempo()
+        {
+            segundos++;
+            if (segundos >= 60)
+            {
+                minutos++;
+                segundos -= 60;
+            }
+        }
+        public string MostrarTiempo()
+        {
+            return $"El tiempo es {minutos} minutos {segundos} segundos."; 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cronometro cronometro = new Cronometro();
+            for(int i=0; i<5000; i++)
+            {
+                cronometro.IncrementarTiempo();
+            }
+            Console.WriteLine(cronometro.MostrarTiempo());
+        }
+    }
+}
---
+++ ejercicioJugador/ejercicioJugador/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ejercicioJugador
+{
+    public interface Jugador
+    {
+        bool Correr(int minutos);
+        bool Cansado();
+        void descansar(int minutos);
+    }
+    public class Amateur : Jugador
+    {
+        public int energia = 20;
+        public int energiaMax = 20;
+        public bool Correr(int minutos)
+        {
+            if (energia >= minutos)
+            {
+                energia -= minutos;
+                return true;
+            }
+            else
+            {
+                energia = 0;
+                return false;
+            }
+        }
+        public bool Cansado()
+        {
+            if (energia > 0)
+                return false;
+            else
+                return true;
+        }
+        public void descansar(int minutos)
+        {
+            energia += minutos;
+            if (energia > energiaMax)
+                energia = energiaMax;
+        }
+    }
+    public class Profesional : Jugador
+    {
+        public int energia = 40;
+        public int energiaMax = 40;
+        public bool Correr(int minutos)
+        {
+            if (energia >= minutos)
+            {
+                energia -= minutos;
+                return true;
+            }
+            else
+            {
+                energia = 0;
+                return false;
+            }
+        }
+        public bool Cansado()
+        {
+            if (energia > 0)
+                return false;
+            else
+                return true;
+        }
+        public void descansar(int minutos)
+        {
+            energia += minutos;
+            if (energia > energiaMax)
+                energia = energiaMax;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+ 
